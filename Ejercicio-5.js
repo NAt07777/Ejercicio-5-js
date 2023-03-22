d4a6d1ceafa63a690d7fd1fb84acd567a1eaa50f
Natalia_Infantino <116117191+NAt07777@users.noreply.github.com>
2023-03-22 15:49:01 -0300

    Add files via upload

---
+++ Ejercicio-5.js
@@ -0,0 +1,13 @@
+let altura_cm = 165;
+let altura_mt = 1.65;
+let peso_kg = 65.5;
+
+let alt_redondeo = Math.ceil(altura_mt);
+console.log(alt_redondeo);
+
+let peso_redondeo = Math.floor(peso_kg);
+console.log(peso_redondeo);
+
+a = Number.MAX_VALUE + 1 === Number.MAX_VALUE;
+
+
